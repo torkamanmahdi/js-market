3692c58f2b0f668011577ff0b6946c2b02f23258
torkamanmahdi <mahditorkaman@pm.me>
2021-09-05 13:52:47 +0430

    forward

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,41 +1,121 @@
-const emptyMessage = document.querySelector('#nullMessage')
-const getRemove = document.querySelector('#removeCart')
-
-const result = await fetch('api/products.json')
-const data = await result.json()
-let products = data.products
-products = products.map( (product) => {
-	const {id} = product
-	const {title, description, category, price} = product.fields
-	const {thumbnail} = product.fields.images
-	return {id, title, description, category, price, thumbnail}
-} )
-
-let cartItem = {
-	id: 0,
-	title: '',
-	price: 2,
-	thumbnail: '',
-	qty: 1
-}
-const getAdd = Array.from(document.getElementsByClassName('buy-this'))
-getAdd.forEach(products => products.addEventListener('click', (e) => {
-	cartItem.id = e.target.parentElement.parentNode.id
-	cartItem.title = e.target.parentElement.parentElement.querySelector('#title').innerText
-	cartItem.price = e.target.parentElement.parentElement.querySelector('strong').innerText
-	cartItem.thumbnail = e.target.parentElement.parentElement.querySelector('img').src
-	localStorage.setItem('cart', JSON.stringify(cartItem))
-
-	let getCartItem = JSON.parse(localStorage.getItem('cart'))
-	emptyMessage.innerHTML = `${getCartItem.title}, price: ${getCartItem.price}`
-}))
-
-if(localStorage.getItem('cart')) {
-	let getCartItem = JSON.parse(localStorage.getItem('cart'))
-	emptyMessage.innerHTML = `${getCartItem.title}, price: ${getCartItem.price}`
-}
-
-getRemove.addEventListener('click', () => {
-	localStorage.removeItem('cart')
-	emptyMessage.innerHTML = 'your cart is empty again'
+const getItem = document.querySelector('#getProducts')
+const cartMessage = document.querySelector('#nullMessage')
+const emptyCart = document.querySelector('#removeCart')
+let iteming = []
+iteming = JSON.parse(localStorage.getItem('cart'))
+console.log(iteming)
+iteming.forEach(element => {
+	cartMessage.innerHTML = `${element.title}`
 })
+
+function eventListeners() {
+	getItem.addEventListener('click', (e) => {
+		getProductItem(e)
+		itemLS()
+	})
+	emptyCart.addEventListener('click', () => {
+		removeLS()
+		cartMessage.textContent = 'cart is empty'
+	})
+	document.addEventListener('DOMContentLoaded', (e) => {
+		if(!localStorage.getItem('cart')) {
+			cartMessage.textContent = 'cart is empty'
+		}
+	})
+}
+eventListeners()
+
+function getProductItem(e) {
+	if(e.target.getElementsByClassName('buy-this')) {
+		const itemProduct = e.target.parentElement.parentElement
+		const itemInfo = {
+			id: itemProduct.id,
+			title: itemProduct.querySelector('#title').innerText,
+			price: itemProduct.querySelector('strong').innerText,
+			thumbnail: itemProduct.querySelector('img').src
+		}
+		addToCart(itemInfo)
+	}
+}
+
+function addToCart(itemInfo) {
+	let row = document.createElement('div')
+	row.innerHTML = `
+		<div>${itemInfo.title}, price: ${itemInfo.price}</div>
+	`
+	cartMessage.appendChild(row)
+	cartToLS(itemInfo)
+}
+
+function cartToLS(item) {
+	let items = cartLS()
+	items.push(item)
+	localStorage.setItem('cart', JSON.stringify(items))
+}
+
+function cartLS() {
+	let items
+	if(localStorage.getItem('cart')) {
+		items = JSON.parse(localStorage.getItem('cart'))
+	} else {
+		items = []
+	}
+	return items
+}
+
+function itemLS() {
+	let LSitems = localStorage.getItem('cart')
+	// console.log(JSON.parse(LSitems))
+}
+
+function removeLS() {
+	if(localStorage.getItem('cart')) {
+		localStorage.removeItem('cart')
+	}
+}
+
+
+
+// const emptyMessage = document.querySelector('#nullMessage')
+// const getRemove = document.querySelector('#removeCart')
+
+// const result = await fetch('api/products.json')
+// const data = await result.json()
+// let products = data.products
+// products = products.map( (product) => {
+// 	const {id} = product
+// 	const {title, description, category, price} = product.fields
+// 	const {thumbnail} = product.fields.images
+// 	return {id, title, description, category, price, thumbnail}
+// } )
+
+// let cartItem = {
+// 	id: 0,
+// 	title: '',
+// 	price: 2,
+// 	thumbnail: '',
+// 	qty: 1
+// }
+// const getAdd = Array.from(document.getElementsByClassName('buy-this'))
+// getAdd.forEach(products => products.addEventListener('click', (e) => {
+// 	cartItem.id = e.target.parentElement.parentNode.id
+// 	cartItem.title = e.target.parentElement.parentElement.querySelector('#title').innerText
+// 	cartItem.price = e.target.parentElement.parentElement.querySelector('strong').innerText
+// 	cartItem.thumbnail = e.target.parentElement.parentElement.querySelector('img').src
+// 	localStorage.setItem('cart', JSON.stringify(cartItem))
+
+// 	let getCartItem = JSON.parse(localStorage.getItem('cart'))
+// 	emptyMessage.innerHTML = `<div>${getCartItem.title}, price: ${getCartItem.price}</div>`
+// }))
+
+// if(localStorage.getItem('cart')) {
+// 	let getCartItem = JSON.parse(localStorage.getItem('cart'))
+// 	emptyMessage.innerHTML = `<div>${getCartItem.title}, price: ${getCartItem.price}</div>`
+// } else {
+// 	emptyMessage.innerHTML = 'empty cart'
+// }
+
+// getRemove.addEventListener('click', () => {
+// 	localStorage.removeItem('cart')
+// 	emptyMessage.innerHTML = 'empty cart'
+// })
